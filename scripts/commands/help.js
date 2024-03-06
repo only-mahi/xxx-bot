95476c4ab563c3420a41d0918f373be4cc9fbccb
mahi-khan0 <161739658+mahi-khan0@users.noreply.github.com>
2024-03-06 20:53:27 +0600

    Update help.js

--- scripts/commands/help.js
+++ scripts/commands/help.js
@@ -1,20 +1,13 @@
-const axios = require('axios');
-const jimp = require("jimp");
-const fs = require("fs");
 module.exports.config = {
-  name: "help",
-  version: "1.0.2",
-  permission: 0,
-  credits: "MAHI",
-  description: "beginner's guide",
-  prefix: true,
-  category: "guide",
-  usages: "[Shows Commands]",
-  cooldowns: 5,
-  envConfig: {
-    autoUnsend: true,
-    delayUnsend: 60
-  }
+	name: 'help',
+	version: '1.0.0',
+	role: 0,
+	hasPrefix: true,
+	category: "guide"
+	aliases: ['help'],
+	description: "Beginner's guide",
+	usage: "Help [page] or [command]",
+	credits: 'Develeoper',
 };
 
 module.exports.run = async function ({
