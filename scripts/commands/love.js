727158b25e567ca513da52e5bf2145d2df8a64aa
mahi-khan0 <161739658+mahi-khan0@users.noreply.github.com>
2024-03-08 11:04:24 +0600

    Update love.js

--- scripts/commands/love.js
+++ scripts/commands/love.js
@@ -7,7 +7,7 @@ module.exports.config = {
   name: "love", 
   version: "1.0.0", 
   permission: 0,
-  credits: "Nayan",
+  credits: "MAHI",
   description: "",
   prefix: true,
   category: "Love", 
