4d7192d8dbff01ea7d7bf931a05ec6a3b274a3a2
mahi-khan0 <161739658+mahi-khan0@users.noreply.github.com>
2024-03-08 13:32:41 +0600

    Update download.js

--- scripts/commands/download.js
+++ scripts/commands/download.js
@@ -1,45 +1,33 @@
-const axios = require('axios');
-const fs = require('fs-extra');
-const d1pt0= require('tinyurl');
 module.exports.config = {
   name: "aldl",
-  version: "1.0.",
-  hasPermssion: 0,
-  credits: "Dipto",
-  description: "All Video Downloader",
-  Category: "other",
-  usages: "fb video link",
-  Prefix: true,
-  cooldowns: 0
+  version: "2.0.0",
+  permission: 0,
+  credits: "MAHI",
+  description: "all video download.",
+  prefix: false,
+  category: "Media",
+  usages: "any video link",
+  cooldowns: 5,
 };
-exports.run = async function ({ api, event, args }) {
-let dipto = args.join(" ") || event.messageReply.attachments[0].url;
-  try {
-if (dipto.startsWith('https://vt.tiktok.com') || dipto.startsWith('https://www.facebook.com') || dipto.startsWith('https://www.instagram.com/') || dipto.startsWith('https://youtu.be/') || dipto.startsWith('https://youtube.com/') || dipto.startsWith('https://x.com/') || dipto.startsWith('https://twitter.com/') || dipto.startsWith('https://vm.tiktok.com') || dipto.startsWith('https://fb.watch')){
-const w = await api.sendMessage("Downloading video, please wait...", event.threadID);
-  if (!dipto) {
-    api.sendMessage("please put a valid fb video link", event.threadID, event.messageID);
-    return;
+
+module.exports.run = async function({ api, event, client, Threads, args }) {
+    const fs = global.nodemodule["fs-extra"], axios = global.nodemodule["axios"], rq = global.nodemodule["request"];
+    
+    if(!args[1]) {
+        var path = __dirname + '';
+        var link = args.slice(0).join("");
     }
-    let path = __dirname + `/cache/AL-DL.mp4`;
-    const aa = await axios.get(`https://d1pt0-all.onrender.com/xnxx?url=${encodeURI(dipto)}`);
-   const bb = aa.data;
-  const uu = await d1pt0.shorten(bb.result);
-    const vid = (await axios.get(bb.result, { responseType: "arraybuffer", })).data;
-    fs.writeFileSync(path, Buffer.from(vid, 'utf-8'));
-  await api.unsendMessage(w.messageID);
-    api.sendMessage({
-      body: `${bb.cp}\n𝗩𝗶𝗱𝗲𝗼 𝗨𝗿𝗹 = ${uu}`,
-      attachment: fs.createReadStream(path) }, event.threadID, () => fs.unlinkSync(path), event.messageID)}
-if (dipto.startsWith('https://i.imgur.com')){
-  const dipto3 = dipto.substring(dipto.lastIndexOf('.'));
-  const response = await axios.get(dipto, { responseType: 'arraybuffer' });
-const filename = __dirname + `/cache/dipto${dipto3}`;
-    fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
-    api.sendMessage({body: `Downloaded from link`,attachment: fs.createReadStream(filename)},event.threadID,
-  () => fs.unlinkSync(filename),event.messageID)
-}
-} catch (e) {
-api.sendMessage(`${e}`, event.threadID, event.messageID);
-  };
-};
+    else {
+        var path = __dirname + '/'+args[0];
+        var link = args.slice(1).join("");
+    };
+    var format = rq.get(link);
+    var namefile = format.uri.pathname;
+    var path = path+'/'+(namefile.slice(namefile.lastIndexOf("/")+1));
+    let getimg = (await axios.get(link, { responseType: "arraybuffer" }))
+    .data;
+  fs.writeFileSync(path, Buffer.from(getimg, "utf-8"));
+  
+  return api.sendMessage("Save the file to the folder"+path, event.threadID, event.messageID);
+    
+}  
